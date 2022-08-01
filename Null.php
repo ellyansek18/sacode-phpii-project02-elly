9e0cf0b7b6c4a8713783fcdda4da43162b57e89a
ellyansek18 <ellyansek18@gmail.com>
2022-08-01 11:14:19 +0900

    first commit

---
+++ Null.php
@@ -0,0 +1,28 @@
+<?php 
+
+	// variabel $salam dengan "TIDAK BERNILAI NULL"
+
+	$salam = '<b>Hello SaCode</b>';
+	echo $salam;
+	var_dump($salam);
+
+	// buat garis
+	echo "<hr>";
+
+
+	// variabel $salam dengan nilai "NULL"
+
+	$salam = null;
+	echo $salam;
+	var_dump($salam);
+
+	// buat garis
+	echo "<hr>";
+
+	// variabel $salam dengan TIDAK BERNIAI NULL
+
+	$salam = '<b>Selamat Pagi</b>';
+	echo $salam;
+	var_dump($salam);
+
+?>
